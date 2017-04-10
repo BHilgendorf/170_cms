1e3422f240f7452098ff7b03bdb4b177a0fa6160
Betsy Hilgendorf <betsy.m.hilgendorf@gmail.com>
2017-04-10 08:00:27 -0600

    through lesson 12

--- cms.rb
+++ cms.rb
@@ -2,10 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require "tilt/erubis"
 require "redcarpet"
-# require "pry"
+# require 'pry'
 
-
-root = File.expand_path("..", __FILE__)
 
 configure do
   enable :sessions
@@ -17,6 +15,14 @@ def render_markdown(text)
     markdown.render(text)
 end
 
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../tests/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
 def load_file(path)
     contents = File.read(path)
     case File.extname(path)
@@ -24,19 +30,54 @@ def load_file(path)
       headers['Content-Type'] = 'text/plain'
       contents
     when ".md"
-      render_markdown(contents)
+      erb render_markdown(contents)
     end
 end
 
+def invalid_name?(file)
+  file.to_s.length <= 0 
+end
+
+def invalid_extension?(ext)
+  !(ext == ".txt" || ext == ".md")
+end
+
 get "/" do
-  @docs = Dir.glob(root + "/data/*")
+  pattern = File.join(data_path, "*")
+
+  @docs = Dir.glob(pattern)
   @docs.map! { |file| File.basename(file)}
 
   erb :index
 end
 
+get "/new" do
+
+  erb :new_document
+end
+
+post "/new" do
+  # binding.pry
+
+  if invalid_name?(params[:filename])
+    session[:message] = "A name is required"
+    status 422
+    erb :new_document
+  elsif invalid_extension?(File.extname(params[:filename]))
+    session[:message] = "Document must be either a '.txt' or '.md' file."
+    status 422
+    erb :new_document
+  else
+    file_path = File.join(data_path, params[:filename])
+    File.write(file_path, "")
+
+    session[:message] = "#{params[:filename]} was created."
+    redirect "/"
+  end
+end
+
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
   if File.exists?(file_path)
     load_file(file_path)
@@ -47,7 +88,7 @@ get "/:filename" do
 end
 
 get "/:filename/edit" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
   @file_name = params[:filename]
   @file_contents = File.read(file_path)
 
@@ -55,9 +96,12 @@ get "/:filename/edit" do
 end
 
 post "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
   File.write(file_path, params[:content])
 
   session[:message] = "#{params[:filename]} has been updated."
   redirect "/"
 end
+
+
+
--- tests/cms_test.rb
+++ tests/cms_test.rb
@@ -1,5 +1,7 @@
 ENV["RACK_ENV"] = "test"
 
+require "fileutils"
+
 require "minitest/autorun"
 require "rack/test"
 
@@ -12,18 +14,36 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)  #data_path method available since defined in global scope 
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+    create_document("about.md")
+    create_document("changes.txt")
+
     get "/"
 
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
     assert_includes(last_response.body, "about.md")
-    assert_includes(last_response.body, "history.txt")
     assert_includes(last_response.body, "changes.txt")
 
   end
 
-  def test_display_file
+  def test_view_text_file
+    # skip
+    create_document("history.txt", "2001: 20th Anniversary")
     get "/history.txt"
 
     assert_equal(200, last_response.status)
@@ -31,7 +51,19 @@ class CMSTest < Minitest::Test
     assert_includes(last_response.body, "2001: 20th Anniversary")
   end
 
+  def test_view_markdown_file
+    # skip
+    create_document("about.md", "<strong>Bold Text</strong><h2>Header</h2>")
+    get "/about.md"
+
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, "<strong>Bold Text</strong>")
+    assert_includes(last_response.body, "<h2>Header</h2>")
+  end
+
   def test_file_not_found_redirect
+    # skip
     get "/nofile.txt"
     assert_equal(302, last_response.status)
 
@@ -40,16 +72,9 @@ class CMSTest < Minitest::Test
     assert_includes(last_response.body, "nofile.txt does not exist")
   end
 
-  def test_view_markdown_file
-    get "/about.md"
-
-    assert_equal(200, last_response.status)
-    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
-    assert_includes(last_response.body, "<h2>Moots Cycles</h2>")
-    assert_includes(last_response.body, "<strong>handcrafting</strong>")
-  end
-
   def test_edit_page
+    # skip
+    create_document("changes.txt")
     get "/changes.txt/edit"
 
     assert_equal(200, last_response.status)
@@ -58,6 +83,7 @@ class CMSTest < Minitest::Test
   end
 
   def test_submit_edit_page
+    # skip
     post "/changes.txt", content: "edited content"
     assert_equal(302, last_response.status)
 
@@ -68,5 +94,47 @@ class CMSTest < Minitest::Test
     get "/changes.txt"
     assert_equal(200, last_response.status)
     assert_includes(last_response.body, "edited content")
+  end
+
+  def test_get_new_document_page
+    get "/new"
+
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "<input")
+    assert_includes(last_response.body, %q(<button type="submit"))
+  end
+
+  def test_create_new_document_with_blank_name
+    post "/new", filename: ""
+
+    assert_equal(422, last_response.status)
+    assert_includes(last_response.body, "A name is required")
+  end
+
+  def test_create_new_document_with_no_extension
+    post "/new", filename: "test"
+
+    assert_equal(422, last_response.status)
+    assert_includes(last_response.body, "Document must be either a '.txt' or '.md' file.")
+  end
+
+  def test_create_new_document_with_invalid_extension
+
+    post "/new", filename: "test.pdf"
+
+    assert_equal(422, last_response.status)
+    assert_includes(last_response.body, "Document must be either a '.txt' or '.md' file.")
+  end
+
+  def test_create_new_document_with_valid_name
+    post "/new", filename: "test.txt"
+    assert_equal(302, last_response.status)
+
+    get last_response["Location"]
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "test.txt was created.")
+
+    get "/"
+    assert_includes(last_response.body, "test.txt")
   end
 end
